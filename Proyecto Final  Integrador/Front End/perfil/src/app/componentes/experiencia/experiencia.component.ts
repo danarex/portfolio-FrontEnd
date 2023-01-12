6b745acf38670a911e9e8c6ebb2dfb7af02e1f33
danarex <dmorandin@gmail.com>
2023-01-12 13:51:26 -0300

    changes

--- Proyecto Final  Integrador/Front End/perfil/src/app/componentes/experiencia/experiencia.component.ts
+++ Proyecto Final  Integrador/Front End/perfil/src/app/componentes/experiencia/experiencia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Experiencia } from 'src/app/model/experiencia.model';
 import { ExperienciaService } from 'src/app/service/experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
 
 
 @Component({
@@ -9,13 +10,34 @@ import { ExperienciaService } from 'src/app/service/experiencia.service';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
-  experiencia:Experiencia[] = [];
-  constructor(public experienciaService:ExperienciaService) { }
+  expe:Experiencia[] = [];
+  constructor(public experienciaService:ExperienciaService, private tokenService: TokenService) { }
+  
+  isLogged = false;
 
+  
   ngOnInit(): void {
-    this.experienciaService.traer().subscribe(data2 => {
-      this.experiencia=data2;
-    });
+    this.cargarExperiencia();
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
   }
 
+  cargarExperiencia(): void {
+    this.experienciaService.traer().subscribe(data => { this.expe = data; })
+  }
+
+  delete(id?: number){
+    if(id != undefined){
+      this.experienciaService.delete(id).subscribe(
+        data => {
+          this.cargarExperiencia();
+        }, err => {
+          alert("No se pudo borrar la experiencia");
+        }
+      )
+    }
+  }
 }
